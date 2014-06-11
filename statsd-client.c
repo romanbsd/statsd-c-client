850b8992411a0db493ee67ff56bcf106552d5920
Gabriel Sosa <sosagabriel@gmail.com>
2014-06-11 18:05:18 -0300

    removed useless code

--- statsd-client.c
+++ statsd-client.c
@@ -56,10 +56,6 @@ statsd_link *statsd_init(const char *host, int port)
     memcpy(&(temp->server).sin_addr, &((struct sockaddr_in*)result->ai_addr)->sin_addr, sizeof(struct in_addr));
     freeaddrinfo(result);
 
-    if (inet_aton(host, &(temp->server).sin_addr) == 0) {
-        perror("inet_aton");
-        return NULL;
-    }
     srandom(time(NULL));
 
     return temp;
